95f7fd9209dd09fdfcfe413ee5d43b273a37a643
timran <imailTahir@gmail.com>
2013-11-21 17:10:42 +0530

    Preference issue

--- popcliqs-web/web/home.tmpl.php
+++ popcliqs-web/web/home.tmpl.php
@@ -90,7 +90,7 @@
   </footer>
 </div>
 
-<!-- Modal
+<!-- Modal-->
 <div class="modal fade" id="mypref" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
